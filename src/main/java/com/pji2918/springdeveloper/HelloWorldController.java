f81d8f8593aa6d35430f6fa4a9918fc85335445a
pji2918 <son77391@gmail.com>
2026-09-03 11:35:21 +0900

    class 3

--- src/main/java/com/pji2918/springdeveloper/HelloWorldController.java
+++ src/main/java/com/pji2918/springdeveloper/HelloWorldController.java
@@ -1,15 +1,36 @@
 package com.pji2918.springdeveloper;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
 
-    // 클라이언트가 "/hello"로 요청을 보내면 해당 메서드 호출
+    // "/hello" 요청을 보내면 hello() 메서드 호출
+    // http://localhost:8080/hello
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    // http://localhost:8080/test -> Hello, everyone!!!!!
+    @GetMapping("/test")
+    public String test() {
+        return "Hello, everyone!!!!!";
+    }
+
+    @PostMapping("/test")
+    public String postTest() {
+        return "Post Test response!!!!!";
+    }
+
+    @DeleteMapping("/test")
+    public String deleteTest() {
+        return "Delete Test response!!!!!";
+    }
+
+    @PutMapping("/test")
+    public String putTest() {
+        return "Put Test response!!!!!";
     }
 }
